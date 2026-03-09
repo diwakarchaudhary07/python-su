067164846231a7d89c7ef2294fecb04f7af0eb12
diwakar chaudhary <boyroyal4853@gmail.com>
2026-03-09 23:53:59 +0530

    add

---
+++ .dist/if_else.py
@@ -0,0 +1,23 @@
+score = 85
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 75:
+    print("Grade: B")
+elif score >= 60:
+    print("Grade: C")
+else:
+    print("Grade: F")
+
+# <-----------------------------------------------------------------------------
+
+num = 12
+
+if num % 2 == 0:
+    if num > 10:
+        print("Even and greater than 10")
+    else:
+        print("Even but not greater than 10")
+else:
+    print("Odd number")
+
