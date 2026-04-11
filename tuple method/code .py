db54c58c917c889f5f51d40626ac372cd1d2b407
diwakar chaudhary <boyroyal4853@gmail.com>
2026-04-11 22:36:00 +0530

    add

---
+++ tuple method/code .py
@@ -0,0 +1,12 @@
+# Starting tuple
+t = (1, 2, 3, 2, 4, 5, 2)
+
+print("Original Tuple:", t)
+
+# 1. count()
+cnt = t.count(2)
+print("Count of 2:", cnt)
+
+# 2. index()
+idx = t.index(4)
+print("Index of 4:", idx)
