85c63f39b7f48a0595ee6ca788e06c571b000f97
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-03 23:52:51 +0530

    add

---
+++ kadanes_algorithm.py
@@ -0,0 +1,8 @@
+def max_subarray(arr):
+    current_sum = max_sum = arr[0]
+    
+    for num in arr[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+    
+    return max_sum
