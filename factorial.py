2f76bcf81f18552ab840696fcef713d8178ae002
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-25 23:40:04 +0530

    add

--- factorial.py
+++ factorial.py
@@ -1,10 +1,18 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
 
 
-# num = int(input("Enter a number: "))
-# print("Factorial of", num, "is:", factorial(num))
-print(factorial(5))
+# # num = int(input("Enter a number: "))
+# # print("Factorial of", num, "is:", factorial(num))
+# print(factorial(5))
+
+x = int(input("enter anumber:"))
+
+fact = 1
+if  x>0:
+    for i in range(x,1 ,-1):
+        fact = fact*i
+print(fact)
