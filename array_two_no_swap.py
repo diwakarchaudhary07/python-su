1fb1162824b088612a1536b76ce35ff7ec7b2ea9
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-25 23:45:48 +0530

    add

---
+++ array_two_no_swap.py
@@ -0,0 +1,10 @@
+# Swap Two Elements in Array
+
+arr = [10, 20, 30, 40]
+
+print("Before Swapping:", arr)
+
+# Swapping first and last element
+arr[0], arr[3] = arr[3], arr[0]
+
+print("After Swapping:", arr)
