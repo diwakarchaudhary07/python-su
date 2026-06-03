3ed7aced554498af6dadb9c0425e9a03b5c4ad5f
diwakar chaudhary <boyroyal4853@gmail.com>
2026-06-03 23:25:56 +0530

    add

---
+++ pratice/string_operation.py
@@ -0,0 +1,6 @@
+# Python Program: Print First 3 Characters, Last 3 Characters, and Reverse String
+s = input("Enter a string: ")
+
+print("First 3 characters:", s[:3])
+print("Last 3 characters:", s[-3:])
+print("Reverse string:", s[::-1])
